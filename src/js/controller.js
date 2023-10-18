750493cca94732879c8decc211ccf23fb7a302fe
slady <csl.bunny.2207@gmail.com>
2023-10-18 22:07:03 +0200

    control recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,7 +30,7 @@ const controlRecipes = async function () {
 
     // Loading recipe
     await model.loadRecipe(id);
-    // await model.loadRecipe(data);
+   
 
     // Rendering recipe
     recipeView.render(model.state.recipe);
